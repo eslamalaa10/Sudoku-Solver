4bf02fe3286057249e6b399145eb79b624748aaa
eslamalaa10 <54743331+eslamalaa10@users.noreply.github.com>
2020-01-13 15:37:01 +0200

    Add files via upload

---
+++ sequential/Sudoku Solver/Sudoku Solver/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Sudoku_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.files_list = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(504, 456);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(523, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // files_list
+            // 
+            this.files_list.FormattingEnabled = true;
+            this.files_list.Items.AddRange(new object[] {
+            "ex1",
+            "ex2",
+            "ex3",
+            "test"});
+            this.files_list.Location = new System.Drawing.Point(524, 42);
+            this.files_list.Name = "files_list";
+            this.files_list.Size = new System.Drawing.Size(74, 21);
+            this.files_list.TabIndex = 2;
+            this.files_list.Text = "ex1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(524, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "solve";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(524, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Time = ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(624, 480);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.files_list);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox files_list;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ sequential/Sudoku Solver/Sudoku Solver/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku_Solver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[,] data;
+
+        //------------------------show the grid on screen-------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            int size = 9;
+            string filemame;
+            filemame = files_list.Text;
+            data = load_file("../../../" + filemame+".txt");
+            for(int i = 0; i < size; i++)
+            {
+                for (int j=0;j<size;j++) {
+                    flowLayoutPanel1.Controls.Add(create_grid(data[i,j]));
+                }
+            }
+        }
+
+        //---------------------------creat one cell and put the value in it---------------------------
+        Label create_grid(int i) {
+            Label l = new Label();
+            l.Name = i.ToString();
+            l.Width = 50;
+            l.Height = 50;
+            l.ForeColor = Color.Blue;
+            if(i==0)
+                l.Text = "";
+            else
+                l.Text = i.ToString();
+            l.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            l.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            return l;
+        }
+
+        //------------------------load the txt file in 2d array----------------------------------------
+        int[,] load_file(String path)
+        {
+            String input = File.ReadAllText(path);
+
+            int i = 0, j = 0;
+            int[,] result = new int[9, 9];
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    result[i, j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+            return result;
+        }
+
+        //-----------------------------find the position of empty cell----------------------------------
+        int[] find_empty() {
+            int[] position=new int[2];
+            int flag = 1;
+           for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9 ; j++)
+                    if (data[i, j] == 0)
+                    {
+                        position[0] = i;
+                        position[1] = j;
+                        flag = 0;
+                        break;   
+                    }
+                if (flag == 0)
+                    break;
+            }
+            if (flag == 1)
+            {
+                position[0] =-1;
+                position[1] = -1;
+                return position;
+            }
+            else
+                return position;
+        }
+
+        //------------------------------------check if the number is valid-------------------------------
+        Boolean valid(int value,int[] position ) {
+            Boolean flag = true;
+            //check the row
+            for(int i = 0; i < 9; i++)
+            {
+                if (data[position[0], i] == value & position[1] != i)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            //check the column
+            for (int i = 0; i < 9; i++)
+            {   if (flag == false)
+                    break;
+
+                else if (data[i, position[1]] == value & position[0] != i)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            //check box
+            int x_box = position[1] / 3;
+            int y_box = position[0] / 3;
+            for(int i= (y_box * 3); i < ((y_box * 3)+3); i++) {
+                if (flag == false)
+                    break;
+                else
+                    for (int j = (x_box * 3); j < ((x_box * 3) + 3); j++)
+                    {
+
+                        if (flag == false)
+                        {
+                            break;
+                        }
+                        else if (data[i, j] == value & position[0] != i & position[1] != j)
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+            }
+            return flag;
+        }
+
+        //----------------------------------solve the puzzel recursively------------------------------------
+        bool solve() {
+            int[] find = find_empty();
+            if(find[0] == -1 & find[1] == -1) //if there is no empty cell so it solved
+            {
+                return true;
+            }
+
+            for(int i = 1; i < 10; i++)
+            {
+                if (valid( i, find))
+                {
+                    data[find[0], find[1]] = i;
+                    if (solve())
+                        return true;
+                    data[find[0], find[1]] = 0;
+                }
+            }
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stopwatch time=Stopwatch.StartNew();
+            Boolean flag = solve();
+            time.Stop();
+            if (!flag)
+                System.Windows.Forms.MessageBox.Show("cant solve");
+            else
+            {
+                flowLayoutPanel1.Controls.Clear();
+                int size = 9;
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        flowLayoutPanel1.Controls.Add(create_grid(data[i, j]));
+                    }
+                }
+                label1.Text = "Time = "+time.Elapsed.TotalSeconds.ToString();
+                System.Windows.Forms.MessageBox.Show("solved :)");
+            }
+
+        }
+
+       
+    }
+}
